1c4e729379e00c1943273f8c2f59a7045f0a13d4
Josiah Sam <josiah.sam@xrtrading.com>
2025-07-15 08:53:01 -0500

    Add none-ls and git integration plugins.

---
+++ lua/plugins/git.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"sindrets/diffview.nvim",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
-  build = ":TSUpdate"
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "cpp", "python" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
